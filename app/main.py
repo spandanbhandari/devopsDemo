864bcbfce3f08f0fca51a0eca193ee72f124ce1a
spandanbhandari <spandanbhandari980@gmail.com>
2026-05-16 21:49:46 +0545

    Pipeline Check

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ REQUESTS = Counter('http_requests_total', 'Total HTTP Requests')
 @app.route('/')
 def hello():
     REQUESTS.inc()
-    return "Hello, DevOps World! CI/CD with GitHub Actions is working. I'm changing something here to test the pipeline."
+    return "Hello, DevOps World! CI/CD with GitHub Actions is working. "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
