bc1373831a644992cb142484f5d7a8979b86b220
spandanbhandari <spandanbhandari980@gmail.com>
2026-05-16 17:36:41 +0545

    Initial commit:Devops Pipeline Setup

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from prometheus_client import make_wsgi_app, Counter
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests')
+
+@app.route('/')
+def hello():
+    REQUESTS.inc()
+    return "Hello, DevOps World! CI/CD with GitHub Actions is working."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
